ec556be094c5967889c6a5d7116bba7247c5f683
faisalalbarkah1414 <faisalalbarkah1414@gmail.com>
2022-10-08 13:47:44 +0700

    Initial Commit

---
+++ ShamoSwiftUI/ShamoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShamoSwiftUIApp.swift
+//  ShamoSwiftUI
+//
+//  Created by faisalalbarkah on 08/10/22.
+//
+
+import SwiftUI
+
+@main
+struct ShamoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
